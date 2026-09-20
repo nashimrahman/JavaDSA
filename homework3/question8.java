0850e2f97f7b8447b0cb7b702a24d9eb7a35af39
nashimrahman <nashimrahman075@gmail.com>
2026-09-21 00:13:26 +0530

    initial commit

---
+++ homework3/question8.java
@@ -0,0 +1,19 @@
+package homework3;
+
+
+public class question8 {
+    public static void main(String[] args) {
+        System.out.println("Print all integers in range from 50 to 100, that are perfectly divisible by 7");
+
+        int n= 100;
+
+        for (int i = 50; i <= n; i++) {
+            if (i%7==0){
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+}
