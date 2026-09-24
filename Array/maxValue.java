bab8eea9d1d1d1d8efb6637782b37ef44316bdd4
nashimrahman <nashimrahman075@gmail.com>
2026-09-25 01:18:42 +0530

    solved

---
+++ Array/maxValue.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class maxValue {
+    static void main(String[] args) {
+        System.out.println("Print the maximum value of an Array");
+
+        int[] arr= {20,7,-9,55,7,0,3};
+        //length of the array
+        int n= arr.length;
+
+
+        //let's assume 0 index has the maxvalue
+        int maxValue = arr[0];
+
+        //comparing maxValue with each element
+        for (int i=0; i<= n-1; i++){
+            if (arr[i]>= maxValue){
+                //then update
+                maxValue= arr[i];
+            }
+        }
+        System.out.println(maxValue);
+
+
+
+
+    }
+}
